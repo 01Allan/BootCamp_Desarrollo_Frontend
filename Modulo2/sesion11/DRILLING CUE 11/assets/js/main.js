d2a3e98bd1f887902098e1baee4fcf9acf32aab0
Allan <allanpinedag@gmail.com>
2023-02-18 17:57:31 -0600

    API's

--- Modulo2/sesion11/DRILLING CUE 11/assets/js/main.js
+++ Modulo2/sesion11/DRILLING CUE 11/assets/js/main.js
@@ -1,9 +1,46 @@
 // API de studio Ghibli
 
-var card = document.getElementById('tarjeta');
+// funciones
+function information(data) {
+    let card_info = document.getElementById("tarjeta");
 
-fetch('https://studio-ghibli-films-api.herokuapp.com/api/My Neighbor Totoro')
+    card_info.innerHTML = 
+    `
+        <div class="tarjeta__info card text-center">
+            <img src="${data.poster}" class="tarjeta__img card-img-top" alt="...">
+            <div class="tarjeta__texto card-body">
+                <h5 class="card-title">${data.title}</h5>
+                <p class="card-text">
+                    ${data.hepburn}
+                </p>
+
+                <p class="card-text">
+                    <strong>Año de estreno: </strong> ${data.release}
+                </p>
+
+                <p class="card-text">
+                    <strong>Director: </strong> ${data.director}
+                </p>
+
+                <h6>sinopsis:</h6>
+                <p class="card-text">
+                    ${data.synopsis}
+                </p>
+
+
+            </div>
+        </div>
+
+
+    `
+
+}
+
+
+fetch('https://studio-ghibli-films-api.herokuapp.com/api/Castle in the sky')
     .then(response => response.json())
     .then(datos => {
-        console.log(datos);
+        information(datos);
     });
+
+
