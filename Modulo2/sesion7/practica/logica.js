8e5758a8b3088b3d990cbe5cabcbc32ec3a1c13c
Allan@alef <apinedag@unah.hn>
2023-03-02 19:46:44 -0600

    ui/ux mod3

---
+++ Modulo2/sesion7/practica/logica.js
@@ -0,0 +1,67 @@
+console.log("helloo xd");
+
+// var numero1 = 5;
+// var numero2 = 10;
+// var resultado = numero1 + numero2;
+// var nombre = "Allan"
+// // IMPRESION CONCATENADA console.log("no se imprime xd")
+// console.log(nombre + " dice que el resultado de la suma es: " + resultado + " xd");
+
+// var numero1, numero2, resultado; 
+// console.log("Suma de dos numeros xd:");
+// numero1 = prompt("Ingrese el primer numero xd:");
+// numero2 = prompt("ingrese el segundo numero xd:");
+// numero1 = parseInt(numero1);
+// numero2 = parseInt(numero2);
+// resultado = numero1 + numero2;
+
+// // alert("La suma de " + numero1 + numero2 + " es " + resultado); 
+
+// function suma(numero1, numero2){
+//     resultado = numero1 + numero2;
+//     return resultado;
+// }
+
+// function resta(numero1, numero2){
+//     resultado = numero1 - numero2;
+//     return resultado;
+// }
+
+// function multiplicacion(numero1, numero2){
+//     resultado = numero1 * numero2;
+//     return resultado;
+// }
+
+// function divicion(numero1, numero2){
+//     resultado = numero1 / numero2;
+//     return resultado;
+// }
+
+// resultado = resta(numero1, numero2);
+// alert("La resta de " + numero1 + " - " + numero2 + " es " + resultado);
+
+var numero = parseInt(prompt("Ingrese un numero: "));
+
+// if (numero == 1) {
+//     alert("Seleccionaste el numero 1, ganaste!");
+// } else {
+//     alert("sigue participando");
+// }
+
+switch (numero) {
+    case 1:
+        alert("La opcion ingresada es 1");
+        break;
+    case 2:
+        alert("La opcion ingresada es dos");
+        break;
+    case 3:
+        alert("la opcion ingresada es tres");
+    default:
+        alert("La opcion ingresada es distinto a 1, 2, o 3");
+        break;
+}
+
+
+
+
