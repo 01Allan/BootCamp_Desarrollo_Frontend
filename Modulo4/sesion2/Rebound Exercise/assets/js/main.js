e0f1b096c1c28d6926b04e9b164ac7a94c89e211
Allan Pineda <apinedag@unah.hn>
2023-04-18 02:09:05 -0600

    go to App Metereologica

--- Modulo4/sesion2/Rebound Exercise/assets/js/main.js
+++ Modulo4/sesion2/Rebound Exercise/assets/js/main.js
@@ -1,24 +1,22 @@
-// numeros
-var uno = document.getElementById("uno");
-var dos = document.getElementById("dos");
-var tres = document.getElementById("tres");
-var cuatro = document.getElementById("cuatro");
-var cinco = document.getElementById("cinco");
-var seis = document.getElementById("seis");
-var siete = document.getElementById("siete");
-var ocho = document.getElementById("ocho");
-var nueve = document.getElementById("nueve");
-var cero = document.getElementById("cero");
-
-// opraciones
-var suma = document.getElementById("suma");
-var resta = document.getElementById("resta");
-var mult = document.getElementById("mult");
-var division = document.getElementById("division");
-
 // otras
+var valores = document.querySelectorAll("#calc");
 var clear = document.getElementById("clear");
 var igual = document.getElementById("igual");
 var escribir = document.getElementById("escribir");
 
-// calculadora 
+// calculadora
+clear.addEventListener("click", () => {
+    escribir.innerText = "";
+});
+
+valores.forEach((valor) => {
+    valor.addEventListener("click", () => {
+        let capturado = "";
+        capturado += valor.textContent;
+        escribir.innerText += capturado;
+    });
+});
+
+igual.addEventListener("click", () => {
+    escribir.innerText = eval(escribir.textContent);
+});
