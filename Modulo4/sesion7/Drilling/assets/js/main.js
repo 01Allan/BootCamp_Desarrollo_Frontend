39c3f5949504092255e44a46ea11d2d24b4b4d9d
Allan Pineda <apinedag@unah.hn>
2023-06-07 17:43:02 -0600

    sesion 7 js

---
+++ Modulo4/sesion7/Drilling/assets/js/main.js
@@ -0,0 +1,25 @@
+// solo para probar defino una funcion para extraer los elementos que contengan id del dom, asi escribo menos jaja y probando a ver si sirve. xd
+function domElementId(element) {
+    return document.getElementById(element);
+}
+
+// valores
+var typeM = domElementId('typeM');
+var presion = domElementId('presion');
+var bloqueMo = domElementId("bloqueMo");
+var lcc = domElementId("lcc");
+var hprpm = domElementId("hprpm");
+var lrrpm = domElementId("lrrpm");
+var sysmart = domElementId("sysmart");
+var inyeccion = domElementId("inyeccion");
+var remoteon = domElementId("remoteon");
+
+const vehiculo = {
+    marca: "",
+    modelo: "",
+    motor: {
+    }
+}
+
+
+
